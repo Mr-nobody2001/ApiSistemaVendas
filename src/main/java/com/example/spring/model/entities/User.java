0c230cdc5f17a62a60cff1917cb7f43ace5b6701
Gabriel Barbosa <gabrielbarbosadev2022@gmail.com>
2023-07-07 12:46:27 -0300

    Classe User e ResourceUser criada. Banco de dados H2 configurado

---
+++ src/main/java/com/example/spring/model/entities/User.java
@@ -0,0 +1,25 @@
+package com.example.spring.model.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
